3aae5b9a64f1317430e658166da4c0b6bf893aff
Marisa Smith <mesmith027@gmail.com>
2020-11-23 12:33:33 -0500

    Creating a Thanksgiving turkey app!

---
+++ turkey/turkey_app.py
@@ -0,0 +1,9 @@
+import streamlit as st 
+
+st.title('Thanksgiving Fun :turkey:')
+st.subheader('The Turkey App!')
+st.write('Use this app to create turkeys!')
+
+st.write(':turkey:')
+
+st.image('img/turkey_basic.png', use_col_width = True)
